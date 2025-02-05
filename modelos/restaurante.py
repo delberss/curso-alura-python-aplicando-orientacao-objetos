002d33cc80d4b066d119b825aa2cab668f1867d7
delberss <delberss@hotmail.com>
2025-02-05 16:02:35 -0300

    proprio metodo para adicionar os restaurantes na classe e metodo para retornar todos os restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,17 +1,20 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria): # self é para referenciar o objeto
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self) # quando criar um objeto, ele vai para a propria lista da classe
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 restaurante1 = Restaurante('Madero', 'Gourmet')
 restaurante2 = Restaurante('Bobs', 'FastFood')
 
-
-restaurantes = [restaurante1, restaurante2]
-
-print((restaurante1))
-print(vars(restaurante2))
+Restaurante.listar_restaurantes()
